199af75a432c81237571aa795e4c3fc6c0fbbf67
alex <aaa@mail.ru>
2023-01-19 22:50:44 +0500

    change 5_coroutines

--- async_molchanov/5_coroutines.py
+++ async_molchanov/5_coroutines.py
@@ -1,7 +1,24 @@
 
 
 def subgen():
-    message = yield
+    x = 'Ready to accept message'
+    message = yield x
     print('Subgen received:', message)
+
+
+def average():
+    count = 0
+    summ = 0
+    average = None
+
+    while True:
+        try:
+            x = yield average
+        except StopIteration:
+            print('Done!')
+        else:
+            count += 1
+            summ += x
+            average = round(summ / count, 2)
 
 
